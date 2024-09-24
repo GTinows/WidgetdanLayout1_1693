750376fc095a32dc487c235202dd6c93f0c2f8d8
GTinows <220711693@students.uajy.ac.id>
2024-09-25 01:01:21 +0700

    GDWidgetdanLayout1
    Mukti Laksono 220711693

---
+++ android/app/src/main/kotlin/com/example/gd1_b_1693/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gd1_b_1693
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:gd1_b_1693/showProfile.dart';
+import 'package:gd1_b_1693/isiLinkTree.dart';
+import 'package:gd1_b_1693/isiLogo.dart';
+import 'package:gd1_b_1693/utilities/constant.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 228, 105, 132),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const ShowProfile(),
+                    ),
+                  );
+                },
+                child: const CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('images/download.jpeg'),
+                ),
+              ),
+              const Text(
+                'Sakshi',
+                style: kTextStyle1,
+              ),
+              const Text(
+                'FLUTTER DEVELOPER',
+                style: kTextStyle2,
+              ),
+              kSizeBox,
+              const IsiLinkTree(),
+              kSizeBox,
+              const IsiLogo(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
